f773e7620af762376f426e40a7478064ff6795a0
tsanchez <v-tsanchez@acvauctions.com>
2025-06-08 01:15:04 -0300

    style: consistent css
    MUI handles standardization. Custom scrollbar.
    
    Signed-off-by: tsanchez <v-tsanchez@acvauctions.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material";
 import { getTheme } from "./theme/theme.js";
 import MainView from "./views/main/MainView.jsx";
@@ -7,7 +6,7 @@ function App() {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
   document.body.classList.remove("bg-light", "bg-dark");
-  document.body.classList.add(prefersDarkMode ? "bg-light" : "bg-dark");
+  document.body.classList.add(prefersDarkMode ? "bg-dark" : "bg-light");
 
   return (
     <ThemeProvider theme={getTheme(prefersDarkMode ? "dark" : "light")}>
